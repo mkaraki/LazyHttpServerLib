d6dec81e263b2543ea9d5813c676c7c5286bcb65
mkaraki <26180919+mkaraki@users.noreply.github.com>
2022-01-30 03:41:22 +0900

    Add project files.

---
+++ LazyHttpServerLib/HttpServer.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+
+namespace LazyHttpServerLib
+{
+    public class HttpServer
+    {
+        private HttpListener listener;
+
+        public HttpServer(IEnumerable<string> prefixes)
+        {
+            listener = new();
+         
+            foreach(string prefix in prefixes)
+                listener.Prefixes.Add(prefix);
+        }
+
+        public void Start()
+        {
+            listener.Start();
+            StartWait();
+        }
+
+        public void Stop()
+        {
+            listener.Close();
+        }
+
+        public event EventHandler<IncomingHttpRequestEventArgs>? IncomingHttpRequest;
+
+        private void StartWait()
+        {
+            listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
+        }
+
+        private void ListenerCallback(IAsyncResult result)
+        {
+            HttpListener? listener = result.AsyncState as HttpListener;
+            if (listener == null) return;
+            HttpListenerContext context = listener.EndGetContext(result);
+            StartWait();
+
+            if (IncomingHttpRequest != null)
+                IncomingHttpRequest(this, new(context.Request, context.Response));
+        }
+    }
+
+    public class IncomingHttpRequestEventArgs : EventArgs
+    {
+        public IncomingHttpRequestEventArgs(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            Request = request;
+            Response = response;
+        }
+
+        public HttpListenerRequest Request { get; set; }
+
+        public HttpListenerResponse Response { get; set; }
+    }
+}
---
+++ SampleWebServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using LazyHttpServerLib;
+using MimeMapping;
+
+HttpServer httpServer = new(new string[] { "http://127.0.0.1:8080/" });
+
+string wwwrootdir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot");
+
+httpServer.IncomingHttpRequest += (sender, e) => {
+    Console.WriteLine("=> {0} {1}", e.Request.HttpMethod, e.Request.RawUrl);
+
+    if (e == null || e.Request == null || e.Request.RawUrl == null)
+        return;
+
+    string req_file = e.Request.RawUrl;
+    if (req_file.EndsWith("/"))
+        req_file += "index.html";
+    req_file = req_file.TrimStart('/');
+
+    string file = Path.Combine(wwwrootdir, req_file);
+
+    int code = File.Exists(file) ? 200 : 404;
+
+    e.Response.StatusCode = code;
+
+    if (File.Exists(file))
+    {
+        FileInfo fi = new(file);
+
+        e.Response.ContentLength64 = fi.Length;
+        e.Response.ContentType = MimeUtility.GetMimeMapping(file);
+
+        using (var fs = File.OpenRead(file))
+        {
+            fs.CopyTo(e.Response.OutputStream);
+        }
+    }
+
+    Console.WriteLine("<= {0}: {1}", code, file);
+
+    e.Response.Close();
+};
+
+httpServer.Start();
+
+while (true)
+{
+    if (Console.ReadLine() == "exit")
+    {
+        httpServer.Stop();
+        break;
+    }
+}
